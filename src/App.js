c2d9fe9d39fe5c7a05b8e8bb63b3c828f6a05bba
Aaro <t1puaa00@students.oamk.fi>
2022-10-11 14:58:58 +0300

    valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './pages/Navbar';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
+import NotFound from './pages/NotFound';
+import Footer from './pages/Footer';
+import { Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <>
+    <Navbar />
+    <Header />
+    <div className='container'>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contactus" element={<ContactUs />} />
+      <Route path="*" element={<NotFound />} />
+      </Routes>
+      </div>
+      <Footer />
+    </>
+);
 }
 
 export default App;
